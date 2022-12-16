8952b661ecb01e3bb21bd5a001251f72ba3bce3a
Veronica <vveronikanika@gmail.com>
2022-12-16 16:48:14 +0200

    refactor: create new components, add some style

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,53 @@
-import { Feedback } from './Feedback/Feedback';
+import React, { Component } from 'react';
+import styles from './App.module.css';
+import { Statistics } from './Statistics/Statistics';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Section } from './Section/Section';
 
-export const App = () => {
-  return (
-    <>
-      <Feedback />
-    </>
-  );
-};
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onLeaveFeedback = option => {
+    this.setState(prevState => ({ [option]: prevState[option] + 1 }));
+  };
+
+  countTotalFeedback = () => {
+    return Object.values(this.state).reduce((acc, num) => acc + num, 0);
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    return Math.round((this.state.good * 100) / this.countTotalFeedback());
+  };
+
+  render() {
+    const options = Object.keys(this.state);
+
+    return (
+      <>
+        <section className={styles.sectionWrapper}>
+          <Section title="Please leave feedback" className={styles.container}>
+            <FeedbackOptions
+              options={options}
+              onLeaveFeedback={this.onLeaveFeedback}
+            />
+          </Section>
+        </section>
+        <section className={styles.sectionWrapper}>
+          <Section title="Statistics" className={styles.container}>
+            <Statistics
+              good={this.state.good}
+              neutral={this.state.neutral}
+              bad={this.state.bad}
+              total={this.countTotalFeedback()}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
+            />
+          </Section>
+        </section>
+      </>
+    );
+  }
+}
---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,20 @@
+import styles from './FeedbackOptions.module.css';
+import PropTypes from 'prop-types';
+
+export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  console.log(options);
+  return (
+    <div className={styles.buttonsList}>
+      {options.map(item => (
+        <button key={item} onClick={() => onLeaveFeedback(item)}>
+          {item}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
+};
